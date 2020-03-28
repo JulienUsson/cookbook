5f889bed504454c364502b43b045d47935bc2a5c
Julien Usson <julien.usson@easymov.fr>
2020-03-28 23:40:17 +0100

    :sparkles: change servings

--- src/components/Ingredients.tsx
+++ src/components/Ingredients.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
-import { Typography } from "@material-ui/core";
+import React, { useState, useMemo } from "react";
+import { Typography, IconButton } from "@material-ui/core";
 import { RecipeIngredient } from "../services/RecipeService";
+import PlusIcon from "@material-ui/icons/Add";
+import MinusIcon from "@material-ui/icons/Remove";
 
 interface Props {
   ingredients: RecipeIngredient[];
@@ -12,12 +14,33 @@ export default function Ingredients({
   servings: servingsProp
 }: Props) {
   const [servings, setServings] = useState<number>(servingsProp);
+  const ingredients = useMemo(
+    () =>
+      ingredientsProp.map(i => ({
+        ...i,
+        quantity: (i.quantity * servings) / servingsProp
+      })),
+    [ingredientsProp, servings, servingsProp]
+  );
 
   return (
     <div>
+      <IconButton
+        aria-label="minus"
+        onClick={() => {
+          if (servings > 1) {
+            setServings(servings - 1);
+          }
+        }}
+      >
+        <MinusIcon />
+      </IconButton>
       <Typography>Personnes: {servings}</Typography>
+      <IconButton aria-label="plus" onClick={() => setServings(servings + 1)}>
+        <PlusIcon />
+      </IconButton>
       <Typography variant="h6">Ingrédients :</Typography>
-      {ingredientsProp.map((ingredient, index) => (
+      {ingredients.map((ingredient, index) => (
         <Typography key={index}>
           {ingredient.quantity}
           {ingredient.unit} {ingredient.name}
