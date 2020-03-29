938a21248051188440c902c7c2b482f6ef2ab49c
Julien Usson <julien.usson@easymov.fr>
2020-03-29 17:57:01 +0200

    :sparkles: recipe detail

--- src/components/Ingredients.tsx
+++ src/components/Ingredients.tsx
@@ -1,8 +1,16 @@
 import React, { useState, useMemo } from "react";
-import { Typography, IconButton } from "@material-ui/core";
+import { Typography, IconButton, styled } from "@material-ui/core";
 import { RecipeIngredient } from "../services/RecipeService";
 import PlusIcon from "@material-ui/icons/Add";
 import MinusIcon from "@material-ui/icons/Remove";
+
+const ServingsSelector = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between",
+  width: 150
+}));
 
 interface Props {
   ingredients: RecipeIngredient[];
@@ -25,24 +33,27 @@ export default function Ingredients({
 
   return (
     <div>
-      <IconButton
-        aria-label="minus"
-        onClick={() => {
-          if (servings > 1) {
-            setServings(servings - 1);
-          }
-        }}
-      >
-        <MinusIcon />
-      </IconButton>
-      <Typography>Personnes: {servings}</Typography>
-      <IconButton aria-label="plus" onClick={() => setServings(servings + 1)}>
-        <PlusIcon />
-      </IconButton>
+      <Typography variant="h6">Personnes :</Typography>
+      <ServingsSelector>
+        <IconButton
+          aria-label="minus"
+          onClick={() => {
+            if (servings > 1) {
+              setServings(servings - 1);
+            }
+          }}
+        >
+          <MinusIcon />
+        </IconButton>
+        <Typography>{servings}</Typography>
+        <IconButton aria-label="plus" onClick={() => setServings(servings + 1)}>
+          <PlusIcon />
+        </IconButton>
+      </ServingsSelector>
       <Typography variant="h6">Ingrédients :</Typography>
       {ingredients.map((ingredient, index) => (
         <Typography key={index}>
-          {ingredient.quantity}
+          - {ingredient.quantity}
           {ingredient.unit} {ingredient.name}
         </Typography>
       ))}
--- src/screens/RecipeDetails.tsx
+++ src/screens/RecipeDetails.tsx
@@ -1,9 +1,69 @@
 import React, { useMemo } from "react";
 import { Recipe, RecipeTag } from "../services/RecipeService";
-import { Card, Typography, IconButton } from "@material-ui/core";
+import {
+  Typography,
+  IconButton,
+  styled,
+  Box,
+  useTheme,
+  useMediaQuery,
+  Button
+} from "@material-ui/core";
 import Ingredients from "../components/Ingredients";
 import EditIcon from "@material-ui/icons/Edit";
 import { Redirect, useParams } from "react-router-dom";
+import Tag from "../components/Tag";
+import { Link } from "react-router-dom";
+import BackIcon from "@material-ui/icons/ArrowBackIos";
+
+const Root = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  [theme.breakpoints.up("sm")]: {
+    height: "100vh",
+    flexDirection: "row"
+  }
+}));
+
+const LeftPanel = styled("div")(({ theme }) => ({
+  padding: theme.spacing(1),
+  display: "flex",
+  flexDirection: "column",
+  [theme.breakpoints.only("xs")]: {
+    margin: theme.spacing(0, 2)
+  },
+  [theme.breakpoints.up("sm")]: {
+    minWidth: 200,
+    marginRight: theme.spacing(2)
+  }
+}));
+
+const LeftPanelContent = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+    justifyContent: "center"
+  }
+}));
+
+const Body = styled("div")(({ theme }) => ({
+  flexGrow: 1,
+  overflowY: "auto",
+  textAlign: "justify",
+  padding: theme.spacing(2)
+}));
+
+const TitleContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row"
+}));
+
+const TagsContainer = styled("div")(({ theme }) => ({
+  "& > *": {
+    marginRight: theme.spacing(1)
+  }
+}));
 
 interface Props {
   recipes: Recipe[];
@@ -12,6 +72,9 @@ interface Props {
 
 export default function RecipeDetails({ recipes }: Props) {
   const { id: recipeId } = useParams();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.only("xs"));
+
   const recipe = useMemo(
     () => recipes.find(r => r.id.toString() === recipeId),
     [recipeId, recipes]
@@ -22,22 +85,71 @@ export default function RecipeDetails({ recipes }: Props) {
   }
 
   return (
-    <Card>
-      <Typography variant="h2">{recipe.name}</Typography>
-      <IconButton
-        component="a"
-        aria-label="edit"
-        href={recipe.issueLink}
-        target="_blank"
-      >
-        <EditIcon />
-      </IconButton>
-      {recipe.duration && <Typography>Durée: {recipe.duration}</Typography>}
-      <Ingredients
-        ingredients={recipe.ingredients}
-        servings={recipe.servings}
-      />
-      <div dangerouslySetInnerHTML={{ __html: recipe.detailsHtml }} />
-    </Card>
+    <Root>
+      {isMobile && (
+        <Box p={2}>
+          <BackToHome />
+          <Title recipe={recipe} />
+        </Box>
+      )}
+      <LeftPanel>
+        {!isMobile && <BackToHome />}
+        <LeftPanelContent>
+          {recipe.duration && (
+            <Typography variant="subtitle1">
+              <Typography variant="h6" component="span">
+                Durée :{" "}
+              </Typography>
+              {recipe.duration}
+            </Typography>
+          )}
+          <Ingredients
+            ingredients={recipe.ingredients}
+            servings={recipe.servings}
+          />
+        </LeftPanelContent>
+      </LeftPanel>
+
+      <Body>
+        {!isMobile && <Title recipe={recipe} />}
+        <Typography dangerouslySetInnerHTML={{ __html: recipe.detailsHtml }} />
+      </Body>
+    </Root>
+  );
+}
+
+function Title({ recipe }: { recipe: Recipe }) {
+  return (
+    <>
+      <TitleContainer>
+        <Typography variant="h2" noWrap>
+          {recipe.name}
+        </Typography>
+        <Box flexGrow={1} />
+        <div>
+          <IconButton
+            component="a"
+            aria-label="edit"
+            href={recipe.issueLink}
+            target="_blank"
+          >
+            <EditIcon />
+          </IconButton>
+        </div>
+      </TitleContainer>
+      <TagsContainer>
+        {recipe.tags.map(tag => (
+          <Tag key={tag.name} {...tag} selected />
+        ))}
+      </TagsContainer>
+    </>
+  );
+}
+
+function BackToHome() {
+  return (
+    <Link to="/">
+      <Button startIcon={<BackIcon />}>Retour</Button>
+    </Link>
   );
 }
